eebabd225d33c83055b495a47394db795b052407
baolong187 <nguyen.bao.long-b@sun-asterisk.com>
2022-07-12 20:52:09 +0700

    chapter 7

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,30 @@
+class UserController < ApplicationController
+  def show
+    @user = User.find_by(params[:id])
+    return if @user
+
+    flash[:danger] = t ".show_user_failed"
+    redirect_to root_url
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      flash[:success] = t "static_pages.home.welcome"
+      redirect_to @user
+    else
+      flash.now[:danger] = t ".alert_not_save"
+      render :new
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
+  end
+end
